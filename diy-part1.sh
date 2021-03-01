71bb6c686fb48bd1e22b418ee7063e3f070f5628
mythbustersjk <65653042+mythbustersjk@users.noreply.github.com>
2021-03-01 15:15:18 +0800

    pre-v0.01

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,7 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+sed -i '$a src-git weifuture https://github.com/hongweifuture/openwrt-packages' .feeds.conf.default
+sed -i '$a src-git pwdep https://github.com/hongweifuture/pwdep.git' .feeds.conf.default
 #添加软件依赖
 #git clone https://github.com/P3TERX/xxx package/xxx
